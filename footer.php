f3901436560949bfebdd9c1f6f5ca2580d5d3632
Abinesh <87063949+abilancer@users.noreply.github.com>
2022-06-26 12:47:44 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,71 @@
+
+<a href="https://api.whatsapp.com/send?phone=+91 99942 00080&text='"  target="_blank" class="fixed group z-50 bottom-12 right-8 bg-emerald-500 md:w-[4rem] md:h-[4rem] w-[2.5rem] h-[2.5rem]  rounded-full drop-shadow-lg flex justify-center items-center text-white md:text-3xl text-2xl hover:bg-emerald-600 hover:drop-shadow-2xl duration-100 md:hover:scale-110 hover:scale-105"><i class="fa-brands fa-whatsapp"></i><span class="tooltip-text bg-emerald-300 p-2 -mt-2 -ml-6 rounded hidden uppercase text-center text-sm text-black group-hover:block absolute  py-2 px-6 z-50  font-semibold">enquire with whatsapp</span></a>
+
+
+
+<footer class="bg-gray-800">
+
+<div class="w-full flex flex-col md:flex-row items-center justify-center px-6 mx-auto pt-10 space-y-4 md:space-y-6  bg-gray-800">
+         <div>
+           <h2 class="max-w-7xl text-3xl  font-black text-center md:text-4xl uppercase text-emerald-500 mb-3">Let's build an understanding</h2>
+           <h4 class="max-w-7xl text-2xl text-center md:text-3xl text-white tracking-wide mb-2">Are you ready for better,detailed customer insights ?</h4>
+           <p class="max-w-3xl text-center text-white mt-1 mb-3 md:text-lg text-base">Stop making bad business decisions that cost you time and money.Talk to our market research specialist to make an informed business decision.</p>
+           <div class="flex justify-center ">
+           <a href="./contact.php" class="py-3 px-8 text-white bg-emerald-700 rounded-full baseline mb-10 hover:bg-slate-900 uppercase tracking-widest">contact us now</a>
+           </div>
+           </div>
+           <!-- <div>
+           <h2 class="max-w-md text-3xl font-black text-center md:text-4xl uppercase text-emerald-500 mb-3">Subscribe for the latest Taxbee updates</h2>
+           <form>
+               <div class="flex justify-center mb-3">
+               <input type=text placeholder="Updated in your inbox" class=" px-3 py-1 rounded-xl focus:outline-none" />
+               </div>
+           </form>
+           <div class="flex justify-center mb-3">
+           <a href="#" class="py-2 px-4 hover:pr-6 text-white bg-emerald-700 rounded-full baseline mb-10 hover:bg-slate-900  uppercase group tracking-widest">submit<i class="fa-solid fa-arrow-right transition ease-out duration-150 group-hover:translate-x-3"></i></a>
+           </div> 
+           </div> -->
+</div>
+
+<div class="px-6 py-7 m-0 w-full mx-auto">
+  
+<div class="flex flex-col md:flex-row justify-evenly  space-y-8 md:space-y-0 ">
+
+  <div class="flex flex-col items-center space-y-3 mb-2 md:w-1/3 text-white ">
+      <h1 class="max-w-md text-2xl font-bold text-emerald-500 text-center md:text-3xl  uppercase">tax<span class="text-white ">bee</span></h1>
+       <div class="flex justify-center space-x-4">
+           <a  href="https://www.facebook.com/" target="_blank"><img src="./assets/img/icons/facebook.svg" alt="" class="h-7 bg-white rounded-full p-1 hover:bg-emerald-500" /></a>
+           <a  href="https://www.instagram.com/" target="_blank"><img src="./assets/img/icons/instagram.svg" alt="" class="h-7 bg-white rounded-full p-1 hover:bg-emerald-500"/></a>
+           <a  href="https://www.linkedin.com/" target="_blank"><img src="./assets/img/icons/linkedin.svg" alt="" class="h-7 bg-white rounded-full p-1 hover:bg-emerald-500"/></a>
+           <a  href="https://www.twitter.com/" target="_blank"><img src="./assets/img/icons/twitter.svg" alt="" class="h-7 bg-white rounded-full p-1 hover:bg-emerald-500"/></a>
+           <a  href="https://www.youtube.com/channel/UCXFusCPSR7EhX6wpOymoyuA" target="_blank"><img src="./assets/img/icons/youtube.svg" alt="" class="h-7 bg-white rounded-full p-1 hover:bg-emerald-500"/></a>
+       </div>
+  </div>
+
+  <div class="flex flex-col items-center space-y-1 mb-2 md:w-1/3 text-white ">
+  <h1 class="max-w-md text-xl font-bold text-emerald-500 text-center md:text-2xl uppercase pb-1">quick links</h1>
+  <a href="./index.php"><span class="text-sm md:text-base hover:text-emerald-500">Home</span></a>
+  <a href="./our_products.php"><span class="text-sm md:text-base hover:text-emerald-500">Our Products</span></a>
+  <a href="./aboutus.php"><span class="text-sm md:text-base hover:text-emerald-500">About Us</span></a>
+  <!-- <a href="./insights.php"><span class="text-sm md:text-base hover:text-emerald-500">Insights</span></a> -->
+  </div>
+
+  <div class="flex flex-col items-center space-y-1 mb-2 md:w-1/3 text-white ">
+  <h1 class="max-w-md text-xl font-bold text-emerald-500 text-center md:text-2xl uppercase pb-1">contacts</h1>
+  <a><span class="text-sm md:text-base"><i class="fa-solid fa-envelope px-2"></i>helpdesk@taxbee.in</span></a>
+  <a><span class="text-sm md:text-base"><i class="fa-solid fa-phone px-2"></i>+91 86 86 09 09 63</span></a>
+  </div>
+
+  </div>
+
+  <div class="flex flex-col-reverse md:justify-between md:flex-row text-white mt-5 px-12 sm:items-center ">
+   <div class="max-w-md text-sm md:text-base text-center">Copyright © 2021 Taxbee. All Rights Reserved.</div></a>
+   <div class="max-w-md text-sm md:text-base mb-2 md:mb-0"><a href="./terms_and_conditions.php"><span>Terms & Conditions</span></a><span class="text-emerald-500 px-2"> | </span><a href="./privacy_policy.php"><span>Privacy Policy</span></a></div>
+  </div>
+
+</div>
+
+</footer>
+    
+
+
---
+++ our_products.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Our Products</title>
+    <link rel="shortcut icon" href="assets/img/logo/favicon.png" />
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+</head>
+<body class="antialiased">
+    <?php include 'header.php' ?>
+
+    <h2 class="text-3xl font-bold md:text-4xl tracking-wide mt-6 mb-1 text-center uppercase">Our products</h2>
+    <h3 class="text-lg tracking-wider font-bold text-center mb-16 md:text-xl capitalize text-emerald-800">A Smooth-Running Tax Is A Relaxing Experience</h3>
+     
+    <div class="container xl:w-[1280px] mx-auto mb-16 text-white">
+      <div class="flex flex-col md:flex-row justify-center space-x-0 md:space-x-4 space-y-4 md:space-y-0">
+
+        <div class="md:w-1/4  w-1/2 h-[30vh] b-0 p-3 self-center rounded-xl relative cursor-pointer bg-emerald-800  hover:bg-slate-900 shadow-2xl flex  justify-center" onclick="location.href='./incorporate_your_business.php';">
+        <div class="text-center  uppercase font-semibold  self-center max-w-md text-xl md:text-2xl">incorporate your bussiness</div>
+        </div>
+        
+        <div class="md:w-1/4  w-1/2 h-[30vh] b-0 p-3 self-center rounded-xl relative cursor-pointer bg-emerald-800  hover:bg-slate-900 shadow-2xl flex  justify-center" onclick="location.href='./gst_services.php';">
+        <div class="text-center  uppercase font-semibold self-center max-w-md text-xl md:text-2xl">gst services</div>
+        </div>
+
+        <div class="md:w-1/4  w-1/2 h-[30vh] b-0 p-3 self-center rounded-xl relative cursor-pointer bg-emerald-800  hover:bg-slate-900 shadow-2xl flex  justify-center" onclick="location.href='./income_tax_returns.php';">
+        <div class="text-center  uppercase font-semibold self-center max-w-md text-xl md:text-2xl">Income Tax Returns</div>
+        </div>
+
+        <div class="md:w-1/4  w-1/2 h-[30vh] b-0 p-3 self-center rounded-xl relative cursor-pointer bg-emerald-800  hover:bg-slate-900 shadow-2xl flex  justify-center" onclick="location.href='./trademarks.php';">
+        <div class="text-center  uppercase font-semibold self-center max-w-md text-xl md:text-2xl">Trademark</div>
+        </div>
+
+      </div>
+    </div>
+  
+    <?php include 'footer.php' ?>
+    <script src="./assets/js/script.js"></script>
+</body>
+</html>
